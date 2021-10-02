4958f5eaf597f207bda1f5f69b7dd7e766c333ec
Nikita Shumeiko <shumeiko.nikita@gmail.com>
2021-10-01 22:56:26 -0400

    Added dockerfiles, renamed frontend to client.

--- download-env.sh
+++ download-env.sh
@@ -4,5 +4,6 @@
 if [ $(id -u) != "0" ]; then
 echo "You must be the superuser to run this script. Please rerun the script using sudo." >&2
 exit 1
+fi
 
-apt-get install docker
+apt-get install docker docker-compose
