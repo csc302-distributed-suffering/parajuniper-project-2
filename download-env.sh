e3aca34815d9717299e48ef3d4643fcae6a12b8e
Nikita Shumeiko <shumeiko.nikita@gmail.com>
2021-10-02 16:39:02 -0400

    Fixed script to (hopefully) be mostly distro-agnostic. Still requires curl

--- download-env.sh
+++ download-env.sh
@@ -6,4 +6,19 @@ echo "You must be the superuser to run this script. Please rerun the script usin
 exit 1
 fi
 
-apt-get install docker docker-compose
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # mac
+    echo "OS is Mac"
+    echo "Checking if homebrew is installed...."
+    if ! [ -x "$(command -v brew)" ]; then
+        echo "Homebrew not found, installing..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    
+    brew cask install docker docker-compose
+else
+    # some unix env
+    trap 'rm -f get-docker.sh' INT EXIT ERR # remove curled script after exiting for any reason
+
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+fi
