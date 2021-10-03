b3f267d8beae105a937feb2b3f591a4eba053b95
liangfe5 <felicia.liang@mail.utoronto.ca>
2021-10-03 00:42:21 -0400

    testing setup

--- src/frontend/src/App.test.js
+++ src/frontend/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { shallow } from "enzyme";
+import { App } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Test App Entry point', function () {
+  it('should have a header tag with Hello world React!', function () {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find("h1").text()).toEqual("Hello world React!");
+  });
 });
