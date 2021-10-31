9bb51a32df09604613dc8637bf2f31c5fd9c2ee7
Eric Wong <ewong211@outlook.com>
2021-10-31 03:27:49 -0400

    Start basic ui

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,31 +2,52 @@ import './App.css';
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-class App extends Component{
+import editIcon from './icons/edit.png';
+import { CardList } from './components/cardlist/cardlist';
+import { Searchbox } from './components/searchbox/searchbox';
+
+class App extends Component {
+  patientlist = [
+    {
+      name: 'John Smith',
+      id: '1',
+      sex: 'Male',
+      birthdate: '09/12/1947'
+    },
+    {
+      name: 'Jean Levin',
+      id: '2',
+      sex: 'Female',
+      birthdate: '02/24/1987'
+    }
+  ]
+
+  constructor() {
+    super();
+    this.state = {
+      patients: this.patientlist
+    }
+  }
+
   render(){
       return (
         <BrowserRouter>
         <Switch>
           <Route exact path='/' render={() => (
             <div className="App">
-              <header className="App-header">
-                <p>
-                  Why, if it isn't a running React app! A truly fine sight indeed.
-                </p>
-                <button onClick={() => {
-                  fetch('/demo/demoRoute')
-                    .then(res => res.json())
-                    .then(json => document.getElementById("hiddenDiv").textContent = json.text)
-                }}>Click Me, I dare you! ;)</button>
-                <div id="hiddenDiv"></div>
-              </header>
+              <div id='searchbar'>
+                <Searchbox type='search' placeholder='What are you looking for today?'/>
+                <button id='editButton' title='Change displayed fields' class="vertical-center">
+                  <img src={editIcon} alt='Edit' id='editIcon'/>
+                  </button>
+              </div>
+              <CardList patients={this.state.patients}/>
             </div>
             )}
           />          
         </Switch>
       </BrowserRouter>
       );
-      
   }
 }
 
