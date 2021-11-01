f83359e0f83bedd486dd1a564157f30918b8fd13
Nikita Shumeiko <shumeiko.nikita@gmail.com>
2021-11-01 15:08:07 -0400

    Added eslint to server

--- src/server/routes/patients.ts
+++ src/server/routes/patients.ts
@@ -1,70 +1,79 @@
 import express = require('express');
-import Client from 'fhir-kit-client'
-import { BundleLink } from 'fhir/r4';
-const router = express.Router()
-const config = { baseUrl: 'https://r3.smarthealthit.org/' };
+import Client from 'fhir-kit-client';
+import {BundleLink} from 'fhir/r4';
+const router = express.Router();
+const config = {baseUrl: 'https://r3.smarthealthit.org/'};
 const fhirKitClient = require('fhir-kit-client');
 const client: Client = new fhirKitClient(config);
 
 
 router.get('/patientInfo', (req, res) => {
-    const patientName = req.body.name ?? "";
-    const count = req.body.count ?? '10'; //TODO: FHIR only supports client-side pagination. So we need to return a link to the next page and the client side needs to make sure it gets loaded. FUN... 
+    const patientName = req.body.name ?? '';
+    const count = req.body.count ?? '10'; // TODO: FHIR only supports client-side pagination. So we need to return a link to the next page and the client side needs to make sure it gets loaded. FUN...
+    const msg = `An error occurred while fetch result for patient ${patientName}.`;
 
-    try{
+    try {
         client.search({
-            resourceType: "Patient",
-            searchParams: {name: patientName, _count: count, _page: "3"} // TODO: how do we deal with name collisions? maybe add diff search params?
+            resourceType: 'Patient',
+            // TODO: how do we deal with name collisions? maybe add diff search params?
+            searchParams: {name: patientName, _count: count, _page: '3'},
 
         }).then((data) => {
-            if(isBundle(data)){
-                const responseData = {nextPageLink: "", totalMatches: 0, returnedMatches: 0, patients: []}
+            if (isBundle(data)) {
+                const responseData = {
+                    nextPageLink: '',
+                    totalMatches: 0,
+                    returnedMatches: 0,
+                    patients: [],
+                };
                 responseData.totalMatches = data.total;
                 responseData.nextPageLink = findNextPageLink(data.link);
-                responseData.returnedMatches = responseData.totalMatches > parseInt(count) ? parseInt(count) : responseData.totalMatches;
+                responseData.returnedMatches = responseData.totalMatches > parseInt(count) ?
+                    parseInt(count) :
+                    responseData.totalMatches;
 
-                for(const entry of data.entry){
-                    if(isPatient(entry.resource)){
+                for (const entry of data.entry) {
+                    if (isPatient(entry.resource)) {
                         responseData.patients.push(entry.resource);
                     }
                 }
 
-                res.status(200).send(responseData)
+                res.status(200).send(responseData);
+            } else {
+                console.warn('Response was not a bundle.');
+                res.status(400).send();
             }
-            else{
-                console.warn("Response was not a bundle.");
-                res.status(400).send;
-            }
-
-        }).catch(e => {
-            console.error(`An error occurred while fetch result for patient ${patientName}.\n`)
-            console.error(e)
-        })
-    } catch(e){
-        console.error(`An error occurred while attempting to fetch data for patient ${patientName}.\n` + e)
+        }).catch((e) => {
+            console.error(`${msg}.\n`);
+            console.error(e);
+            res.status(500).send(msg);
+        });
+    } catch (e) {
+        console.error(`${msg}.\n`);
+        console.error(e);
+        res.status(500).send(msg);
     }
-})
+});
 
 
-//---------------- Util Methods ----------------
+// ---------------- Util Methods ----------------
 const findNextPageLink = (links: BundleLink[]) => {
-    for(const link of links){
-        if(link.relation === "next"){
+    for (const link of links) {
+        if (link.relation === 'next') {
             return link.url;
         }
     }
 
-    return "";
-}
+    return '';
+};
 
-
-//---------------- Type Guards ----------------
+// ---------------- Type Guards ----------------
 const isPatient = (resource: fhir4.Resource): resource is fhir4.Patient => {
     return resource.resourceType === 'Patient';
-}
+};
 
 const isBundle = (resource: fhir4.Resource): resource is fhir4.Bundle => {
     return resource.resourceType === 'Bundle';
-}
+};
 
-module.exports = router
+module.exports = router;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,31 +1,28 @@
 import * as express from 'express';
-const path = require('path')
+const path = require('path');
 
 require('source-map-support').install();
 
-require('dotenv').config()
+require('dotenv').config();
 
-//CONFIG
+// CONFIG
 const app = express();
 
-app.set("port", process.env.PORT || 5000);
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
+app.set('port', process.env.PORT || 5000);
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 
-//routes config
-const patientRoutes = require('./routes/patients')
-app.use('/patients', patientRoutes)
+// routes config
+const patientRoutes = require('./routes/patients');
+app.use('/patients', patientRoutes);
 
 
-//SERVER
-if(process.env.NODE_ENV === 'production'){
+// SERVER
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.listen(app.get('port'), () => {
-    console.log(`Express server is listening on: ${app.get('port')}`)
+    console.log(`Express server is listening on: ${app.get('port')}`);
 });
-
-
-
