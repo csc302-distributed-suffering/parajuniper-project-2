632e8c4fab0ab78158a12e4f9bbc5d924e2bd05a
Viktar Chyhir <vchigir12@gmail.com>
2021-11-03 15:34:14 -0400

    Add identifier props to searchbox and add input handling

--- src/client/src/components/searchbox/searchbox.jsx
+++ src/client/src/components/searchbox/searchbox.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import './searchbox.css';
 
 export const Searchbox = props => {
+    const {onInputChange, name, id, placeholder} = props;
+
     return(
         <input 
             className='searchbox'
-            placeholder='What are you looking for today?'
+            name={name}
+            id={id}
+            placeholder={placeholder}
+            onChange={(e) => {onInputChange(e)}}
         />
     )
 }
