df6627ba87452f1e34d7d26298b2c0f1b03adb2d
Viktar Chyhir <vchigir12@gmail.com>
2021-11-03 15:33:40 -0400

    Add patient actions to client

---
+++ src/client/src/actions/patients.js
@@ -0,0 +1,57 @@
+
+
+const getPatientsWName = async (firstName, lastName, count) => {    
+    
+    const url = `/patients/list?name=${firstName}&${lastName}&count=${count}`
+    const request = new Request(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json text/plain, */*',
+            'Content-Type': 'application/json'
+        }
+    })
+
+    try {
+        const response = await fetch(request)
+        const jsonData = await response.json()
+
+        return {status: response.status, data: jsonData}
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const getPatient = async (id, count) => {    
+    
+    const url = `/patients/info?id=${id}&count=${count}`
+    const request = new Request(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json text/plain, */*',
+            'Content-Type': 'application/json'
+        }
+    })
+
+    try {
+        const response = await fetch(request)
+        const jsonData = await response.json()
+
+        return {status: response.status, data: jsonData}
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+
+
+export {getPatientsWName}
+
+
+
+
+
+
+
+
+
+
