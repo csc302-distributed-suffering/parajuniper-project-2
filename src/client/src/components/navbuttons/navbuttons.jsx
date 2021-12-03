58f275dc24b531481920b44115a4bc9f69bb873e
Eric Wong <ewong211@outlook.com>
2021-12-03 15:39:55 -0500

    Add navigation css

--- src/client/src/components/navbuttons/navbuttons.jsx
+++ src/client/src/components/navbuttons/navbuttons.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './navbuttons.css';
 
 export class Navbuttons extends React.Component {
     constructor(props) {
@@ -6,11 +7,13 @@ export class Navbuttons extends React.Component {
     }
 
     render = () => {
+        const disablePrev = this.props.page === 1;
+        const disableNext = !this.props.nextLink;
         if (this.props.searchResult & this.props.patientNum != 0) {
             return(
-                <div>
-                    <button onClick={this.props.handlePrev} disabled={this.props.page === 1}>Previous</button>
-                    <button onClick={this.props.handleNext} disabled={!this.props.nextLink}>Next</button>
+                <div className='navbuttons'>
+                    <button className='navb prev' onClick={this.props.handlePrev} disabled={disablePrev}>&laquo; Previous</button>
+                    <button className='navb next' onClick={this.props.handleNext} disabled={disableNext}>Next &raquo;</button>
                 </div>
             );
         } else {
