76f8b619ac658cffa1198f8fec4eb0cfb899ed3b
liangfe5 <felicia.liang@mail.utoronto.ca>
2021-12-03 19:48:58 -0500

    temp push

--- src/client/src/components/patientmodal/Modal.js
+++ src/client/src/components/patientmodal/Modal.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Select from 'react-select';
 import './Modal.css';
+import './table.scss';
+import GROUP_MAPS from'./group_maps';
+import Table from 'rc-table';
 
 export class Modal extends React.Component {
   constructor(props) {
@@ -12,6 +15,7 @@ export class Modal extends React.Component {
       options: groupOptions,
       selectedGroup: groupOptions[0]
     };
+    
   }
 
   getPath = (obj, path = "") => {
@@ -45,6 +49,8 @@ export class Modal extends React.Component {
         }
         groups[resourceType].push(entry)
     })
+    console.log('here')
+    console.log(groups)
     return groups
   }
 
@@ -62,6 +68,77 @@ export class Modal extends React.Component {
   handleSelection = e => {
     this.setState({selectedGroup: e})
   }
+
+  generate_table = (group) => {
+    if (Object.keys(group).length === 0){
+      return
+    }
+    var columns = []
+    var column_values = GROUP_MAPS[group.value]
+    column_values.forEach(c_name => {
+      columns.push({
+          title: c_name,
+          dataIndex: c_name,
+          key: c_name,
+          width: 100 
+      }
+      )
+    });
+    var rows_data = this.state.groups[group.value]
+    // console.log('rows data')
+    // console.log('rows_data ' + JSON.stringify(rows_data, null, 4))
+    // console.log('this.state.groups ' + JSON.stringify(this.state.groups, null, 4))
+    // console.log('group.value ' + JSON.stringify(group.value, null, 4))
+    rows_data.forEach(row_value => {
+      // console.log('row value')
+      // console.log(row_value)
+      console.log('row.value[resource] ' + JSON.stringify(row_value['resource'], null, 4))
+      var entry = {}
+      var row_res = row_value['resource']
+      column_values.forEach(c_name => {
+        console.log('c_name ' + JSON.stringify(c_name, null, 4))
+        console.log(row_res[c_name])
+        // console.log('row_res ' + JSON.stringify(row_res, null, 4))
+        // console.log('row_res[c_name]' + JSON.stringify(row_res.c_name, null, 4))
+        // entry[c_name] = row_value['resource'][c_name]
+      })
+      rows_data.push(entry)
+    })
+    var data = {}
+    //temp data below
+    const columns2 = [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        width: 100,
+      },
+      {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+        width: 100,
+      },
+      {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+        width: 200,
+      },
+      {
+        title: 'Operations',
+        dataIndex: '',
+        key: 'operations',
+        render: () => <a href="#">Delete</a>,
+      },
+    ];
+    
+    const data2 = [
+      { name: 'Jack', age: 28, address: 'some where', key: '1' },
+      { name: 'Rose', age: 36, address: 'some where', key: '2' },
+    ];
+    return <Table columns={columns2} data={data2} />
+  }
   
   render = () => {
     const {patient_info, handleClose, show} = this.props;
@@ -75,7 +152,8 @@ export class Modal extends React.Component {
           <div className="patient-field">gender: {patient_info.gender} </div>
           <div className="patient-field">birthdate: {patient_info.birthdate} </div>
           <Select options={this.state.options} value={this.state.selectedGroup} onChange={this.handleSelection}/>
-          <button className="button" type="button" onClick={handleClose}>
+          {this.generate_table(this.state.selectedGroup)}
+          <button className="button open_button" type="button" onClick={handleClose}>
             Close
           </button>
           </div>
---
+++ src/client/src/components/patientmodal/group_maps.js
@@ -0,0 +1,10 @@
+const GROUP_MAPS = {
+    // 'Patient': ['birthDate', 'gender', 'id'],
+    'Patient': ["id"],
+    'CarePlan': ["id", "status"]
+
+}
+
+
+
+export default GROUP_MAPS
