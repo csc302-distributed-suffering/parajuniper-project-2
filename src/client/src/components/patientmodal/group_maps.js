71cbaba34e1d13babc34eff9f9b3dda03fb74fec
liangfe5 <felicia.liang@mail.utoronto.ca>
2021-12-03 20:36:11 -0500

    add comments

--- src/client/src/components/patientmodal/group_maps.js
+++ src/client/src/components/patientmodal/group_maps.js
@@ -1,6 +1,9 @@
+// format: group_name: [[column_name, column width]]
+// put column width as 0 if you want the default [100]
+// for columns like id they should be wider
+
 const GROUP_MAPS = {
     'Patient': [['birthDate', 0], ['gender', 0], ['id', 300]],
-    //'Patient': ["id"],
     'CarePlan': [["id", 300], ["status", 0]]
 
 }
